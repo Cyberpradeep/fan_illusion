59ab9d46a508c3c62a054c422b0b29bf7c1268cf
Pradeep_Selladurai <152625492+Cyberpradeep@users.noreply.github.com>
2025-03-19 18:34:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var img = document.getElementById("image");
+var on = document.getElementById("on");
+var off = document.getElementById("off");
+
+function on_s(){
+    img.classList.add("rotate");
+}
+function off_s(){
+    img.classList.remove("rotate");
+}
